06532548ac94ad547fbedffd41805638d172a0d8
Ycanga <canayunus@gmail.com>
2022-11-29 11:22:05 +0300

    .

--- index.php
+++ index.php
@@ -1,97 +1,185 @@
-<?php
-// Listening submit button
-if (isset($_POST['submit'])) {
-
-// The curl function is included in the project
-    $curl = curl_init();
-// Curl array is created
-curl_setopt_array($curl, [
-    // API information is entered
-    CURLOPT_URL => "https://phonenumbervalidatefree.p.rapidapi.com/ts_PhoneNumberValidateTest.jsp?number=%2B".$_POST['number']."&country=".$_POST['country'],
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_ENCODING => "",
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 30,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => "GET",
-    CURLOPT_HTTPHEADER => [
-        "X-RapidAPI-Host: phonenumbervalidatefree.p.rapidapi.com", // 
-        "X-RapidAPI-Key: 4af258ff08mshe7fe29f940c5294p12e13ejsn5a3d8ec6077e"
-        // Please sign up and get API key from the link I gave in the description. Otherwise the API will not work.
-    ],
-]);
-
-// With the curl function, the API query is executed and kept in the variable.
-$response = curl_exec($curl);
-//The error description that occurs as a result of the query executed is kept in the variable.
-$err = curl_error($curl);
-//The curl function is terminated
-curl_close($curl);
-// If an error has occurred, an if is entered, otherwise the process continues.
-if ($err) {
-    // The error message for the resulting error value is kept in the message variable and printed on the screen.
-    $request = "<div class='alert alert-danger container mt-5' role='alert'>* The entered number is fake or does not work. ! Error request: ".$err."</div>";
-} 
-else {
-    // Informed that the query has run successfully
-     $request = "<div class='alert alert-success container mt-5' role='alert'>*The number entered is real and working</div>";
-     // The query result returns json, this data is decoded for ease of access.
-     $values = json_decode($response, FALSE);
-     // Sending fake numbers into the query is checked.
-     if($values->phoneNumberEntered == '')
-       $request = "<div class='alert alert-danger container mt-5' role='alert'>* The entered number is fake or does not work. ! Error request: ".$err."</div>";
-    }
-}
-?>
-
-<!-- Default form head -->
+<html>
 <head>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <title>Site Yapım Aşamasında</title>
+    <link rel="icon" type="image/png" href="logo1.png"/>
 </head>
+<body>
 
-<!-- Default form input -->
-<body class="bg-dark text-white" >
-    <div class="container">
-        <!-- Notifications are made, such as error messages resulting from the query. -->
-        <?php echo $request;?>
-        <form class="text-center p-5" action="" method="Post">
-
-            <p class="h4 mb-4">Number Check</p>
-
-            <!-- Number -->
-            <input type="number" name="number" class="form-control mb-4" placeholder="Number" required>
-
-            <!-- Country -->
-                <select class="form-control mb-4" name="country">
-                    <option value="NA" selected>Please Select Number Country</option>
-                    <option value="TR">Turkey</option>
-                    <option value="USA">United States</option>
-                </select>
-            <!-- Check button -->
-            <button class="btn btn-info btn-block my-4" name="submit" type="submit">Check</button>
-        </form>
-  <!-- Check Result -->
-  <div class='alert alert-success container mt-5' role='alert'>
-      <b>Entered Number: </b><?php echo $values->phoneNumberEntered; ?><br>
-      <b>Country Code: </b><?php echo $values->countryCode; ?><br>
-      <b>Default Country Entered: </b><?php echo $values->defaultCountryEntered; ?><br>
-      <b>Phone Number Region: </b><?php echo $values->phoneNumberRegion; ?><br>
-      <b>Location: </b><?php echo $values->location; ?><br>
-      <b>Time Zone: </b><?php echo $values->timeZone_s; ?><br>
-      <b>Carrier: </b><?php echo $values->carrier; ?><br>
-  </div>
-
+<div class="main-wrapper">
+<div class="signboard-wrapper">
+<div class="signboard"><img src="logo1.png" style="width:200px"> Bakımdayız</div>
+<div class="string"></div>
+<div class="pin pin1"></div>
+<div class="pin pin2"></div>
+<div class="pin pin3"></div>
 </div>
-
-<!-- Required javascript libraries -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-
+</div>   
+<div class="aciklama">
+    <h1>pra.4lphasoftware.com</h1>
+    <h2>Bakım çalışmaları devam ediyor.</h2>
+    <p>Lütfen daha sonra tekrar ziyaret edin.</p>
+    <p>İletişim: <a href="mailto://info@4lphasoftware.com">info@4lphasoftware.com</a></p>
+    <p><a href="https://www.4lphasoftware.com/">https://www.4lphasoftware.com/</a></p>
+    
+</div>    
 </body>
-<footer>
-    <div class="container-fluid footer fixed-bottom border-top border-white p-3 text-center mt-5">
-        Designed by <b ><a href="https://github.com/ycanga" style="color:#131313; text-decoration: none;">Yunus Emre Canğa</a> | <a href="https://4lphasoftware.com" style="color:#ffb600; text-decoration: none;">4LPHA Software</a></b>
-    </div>
-</footer>
+</html>
+
+<style>
+    @import url("https://fonts.googleapis.com/css?family=Montserrat:400,400i,700");
+
+html,
+body,
+.main-wrapper {
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+
+.main-wrapper {
+  font-size: 10vmin;
+  background-color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: auto;
+  float: right;
+  z-index: 999;
+  position: fixed;
+}
+
+.aciklama {
+  color: #ffffff;
+  font-family: Montserrat, sans-serif;
+  font-size: 3vmin;
+  background-color: black;
+  height: auto;
+  display: block, flex;
+  position: absolute;
+  text-shadow: 0 -0.015em #be2b00;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  width: 100%;
+  bottom: 0;
+  text-decoration: none;
+}
+
+.aciklama a {
+  text-decoration: none;
+  color: #ffffff;
+  font-family: Montserrat, sans-serif;
+  font-size: 3vmin;
+}
+
+
+.signboard-wrapper {
+  width: 105vmin;
+  height: 55vmin;
+  position: relative;
+  flex-shrink: 0;
+  transform-origin: center 2.5vmin;
+  animation: 1000ms init forwards, 1000ms init-sign-move ease-out 1000ms, 3000ms sign-move 2000ms infinite;
+}
+
+.signboard-wrapper .signboard {
+  color: #ffffff;
+  font-family: Montserrat, sans-serif;
+  font-weight: bold;
+  background-color: #ff5625;
+  width: 100vmin;
+  height: 35vmin;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  bottom: 0;
+  border-radius: 4vmin;
+  text-shadow: 0 -0.015em #be2b00;
+}
+
+.signboard-wrapper .string {
+  width: 30vmin;
+  height: 30vmin;
+  border: solid 0.9vmin #893d00;
+  border-bottom: none;
+  border-right: none;
+  position: absolute;
+  left: 50%;
+  transform-origin: top left;
+  transform: rotatez(45deg);
+}
+
+.signboard-wrapper .pin {
+  width: 5vmin;
+  height: 5vmin;
+  position: absolute;
+  border-radius: 50%;
+}
+
+.signboard-wrapper .pin.pin1 {
+  background-color: #9f9f9f;
+  top: 0;
+  left: calc(50% - 2.5vmin);
+}
+
+.signboard-wrapper .pin.pin2,
+.signboard-wrapper .pin.pin3 {
+  background-color: #d83000;
+  top: 21.5vmin;
+}
+
+.signboard-wrapper .pin.pin2 {
+  left: 13vmin;
+}
+
+.signboard-wrapper .pin.pin3 {
+  right: 13vmin;
+}
+
+@keyframes init {
+  0% {
+    transform: scale(0);
+  }
+
+  40% {
+    transform: scale(1.1);
+  }
+
+  60% {
+    transform: scale(0.9);
+  }
+
+  80% {
+    transform: scale(1.05);
+  }
+
+  100% {
+    transform: scale(1);
+  }
+}
+
+@keyframes init-sign-move {
+  100% {
+    transform: rotatez(3deg);
+  }
+}
+
+@keyframes sign-move {
+  0% {
+    transform: rotatez(3deg);
+  }
+
+  50% {
+    transform: rotatez(-3deg);
+  }
+
+  100% {
+    transform: rotatez(3deg);
+  }
+}
+
+</style>
